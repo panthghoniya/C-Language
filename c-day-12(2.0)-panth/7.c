0d8e46d467a8010bf7edf727c18ec4182746c9f5
panthghoniya <ghoniyapanth5@gmail.com>
2024-09-13 09:24:48 +0530

    Add files via upload

---
+++ c-day-12(2.0)-panth/7.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    for (int row = 5; row >= 1; row--)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            printf("%d ",col);
+        }
+        for (int spa = row; spa <= 4; spa++)
+        {
+            printf("    ");
+        }
+        
+        for (int col = row; col >= 1; col--)
+        {
+            printf("%d ",col);
+        }
+        
+        printf("\n");
+    }
+    
+}
---
+++ c-day-12-panth/10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+     for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 5; col >= row; col--)
+        {
+            printf("%d ",row);
+        }
+        printf("\n");
+    }
+}
